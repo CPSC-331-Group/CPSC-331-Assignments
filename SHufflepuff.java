3c9b93ad1c09a08b2145ac448ad317d272bdcbc3
William Chan <willchan789@gmail.com>
2018-09-21 18:19:18 -0600

    added more comments and assertions to slow algorithm

--- SHufflepuff.java
+++ SHufflepuff.java
@@ -2,6 +2,7 @@ import cpsc331.A1;
 
 public class SHufflepuff {
     protected static int sHuffle(int n){
+      // Assertion: A non-negative integer n has been given as input.
       if (n == 0) {
         return 10;
       } else if (n == 1) {
@@ -13,10 +14,13 @@ public class SHufflepuff {
       } else {
         return 4*sHuffle(n - 1) - 6*sHuffle(n - 2) + 4*sHuffle(n - 3) - sHuffle(n - 4);
       }
+      // Assertion:
+      // 1. A non-negative integer n has been given as input.
+      // 2. The nth Hufflepuff number, Hn, has been returned as output.
     }
     public static void main(String[] args) {
-	  try{
-		 Integer.parseInt(args[0]);
+	  try{       //Try the following code
+		  Integer.parseInt(args[0]);
       if (args.length > 0 && args[0].matches("\\d+")) {
         System.out.println(sHuffle(Integer.parseInt(args[0])));
       } else if (Integer.parseInt(args[0]) < 0) {
