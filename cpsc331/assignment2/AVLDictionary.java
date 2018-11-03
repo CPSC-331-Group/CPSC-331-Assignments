b100f94113e28a7e7a591203b2a1ea1e7c6d3656
Betty <zhangbh0605@gmail.com>
2018-11-03 16:22:46 -0600

    dying

--- cpsc331/assignment2/AVLDictionary.java
+++ cpsc331/assignment2/AVLDictionary.java
@@ -281,6 +281,7 @@ public class AVLDictionary<K extends Comparable<K>, V>
   
   private void adjustAVLBalance(AVLNode x){
 	AVLNode problemNode = checkForProblem(x);
+	System.out.println(problemNode.value());
 	if(problemNode != null){
 		if(problemNode.balanceFactor() == 2){
 			AVLNode xLeft = x.left();
