e5eeb8e597f548dc068cbf3baff186b3ec908a64
Betty <zhangbh0605@gmail.com>
2018-11-03 16:27:16 -0600

    dying

--- cpsc331/assignment2/AVLDictionary.java
+++ cpsc331/assignment2/AVLDictionary.java
@@ -285,6 +285,7 @@ public class AVLDictionary<K extends Comparable<K>, V>
 		System.out.println(problemNode.value());
 		if(problemNode.balanceFactor() == 2){
 			AVLNode xLeft = x.left();
+			System.out.println(xLeft.height());
 			if (xLeft.balanceFactor() == 1){
 				rotateRight(problemNode);	
 			}else if(xLeft.balanceFactor() == -1){
@@ -310,7 +311,6 @@ public class AVLDictionary<K extends Comparable<K>, V>
 		//int leftHeight = (x.left()).height();
 		//x.height = Math.max(rightHeight, leftHeight) +1;
 		if(x.balanceFactor() < -1 || x.balanceFactor() > 1){
-			System.out.println(x.value());
 			return x;
 		}
 		x = x.parent();
