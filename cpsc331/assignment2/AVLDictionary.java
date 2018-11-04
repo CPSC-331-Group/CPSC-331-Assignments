c153ff91e519dca56c47ba0193582a880d09ef3d
Betty <zhangbh0605@gmail.com>
2018-11-03 20:52:37 -0600

    buggy

--- cpsc331/assignment2/AVLDictionary.java
+++ cpsc331/assignment2/AVLDictionary.java
@@ -434,6 +434,8 @@ public class AVLDictionary<K extends Comparable<K>, V>
          } else {  // x is a right child
            parent.right = null;
          };
+		 parent.height = 0;
+
      
        };
    
