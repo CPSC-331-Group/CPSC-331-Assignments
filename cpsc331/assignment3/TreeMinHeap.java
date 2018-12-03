4ecfe82b3fd1cc01f7f38f22bad3607be52701fe
William Chan <willchan789@gmail.com>
2018-12-03 14:33:55 -0700

    BubbleUP and BubbleDOWN

--- cpsc331/assignment3/TreeMinHeap.java
+++ cpsc331/assignment3/TreeMinHeap.java
@@ -23,9 +23,9 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
   // b) parent, leftChild and rightChild are (possibly null) references to other
   //    TreeNodes; indx is the position where the data at this TreeNode would be
   //    stored in an ArrayList-based implementation of the same MinHeap
-  
+
   class TreeNode {
-  
+
     // Data Fields
     private T value;             // Value stored at this TreeNode
     private int index;           // Position of this node in an
@@ -33,7 +33,7 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
     private TreeNode parent;     // The parent of this TreeNode
     private TreeNode leftChild;  // The left child of this TreeNode
     private TreeNode rightChild; // The right child of this TreeNode
-    
+
     // Creates a new TreeNode with a given value, "inputValue" and a given
     // index, "inputIndex"
     //
@@ -43,62 +43,62 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
     // Postcondition:
     // a) A TreeNode storing value "inputValue" and with index "inputIndex",
     //    for which parent, leftChild and rightChild are null, has been created.
-    
+
     public TreeNode (T inputValue, int inputIndex) {
-    
+
       value = inputValue;
       index = inputIndex;
       parent = null;
       leftChild = null;
       rightChild = null;
-    
+
     }
-    
+
     // Reports the index of this node.
     //
     // Precondition: The TreeNode Invariant is satisfied.
     // Postcondition: The index of this TreeNode is returned as output.
-    
+
     public int getIndex() {
       return index;
     }
-    
+
     // Reports the value stored at this TreeNode
     //
     // Precondition: The TreeNode Invariant is satisfied.
     // Postcondition: The value stored at this TreeNode is returned as output.
-    
+
     public T getValue() {
       return value;
     }
-    
+
     // Reports the parent of this TreeNode
     //
     // Precondition: The TreeNode Invariant is satisfied.
     // Postcondition: The parent of this TreeNode is returned as output.
-    
+
     public TreeNode getParent () {
       return parent;
     }
-    
+
     // Reports the left child of this TreeNode
     //
     // Precondition: The TreeNode Invariant is satisfied.
     // Postcondition: The left child of this TreeNode is returned as output.
-    
+
     public TreeNode getLeft () {
       return leftChild;
     }
-    
+
     // Reports the right child of this TreeNode
     //
     // Precondition: The TreeNode Invariant is satisfied.
-    // 
-    
+    //
+
     public TreeNode getRight() {
       return rightChild;
     }
-    
+
     // Sets the value stored at this TreeNode
     //
     // Precondition:
@@ -106,11 +106,11 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
     // b) A non-null value inputValue, with type T, is given as input.
     // Postcondition:
     // a) The value stored at this TreeNode is now the given inputValue.
-    
+
     public void setValue(T inputValue) {
       value = inputValue;
     }
-    
+
     // Sets the parent of this TreeNode
     //
     // Precondition:
@@ -118,11 +118,11 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
     // b) A TreeNeed, inputParent, is given as input.
     // Postcondition:
     // a) The parent of this TreeNode is now the given inputParent.
-    
+
     public void setParent(TreeNode inputParent) {
       parent = inputParent;
     }
-    
+
     // Sets the left child of this TreeNode
     //
     // Precondition:
@@ -130,11 +130,11 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
     // b) A TreeNode, inputLeft, is given as input.
     // Postcondition:
     // a) The left child of this TreeNode is now the given inputLeft
-    
+
     public void setLeft(TreeNode inputLeft) {
       leftChild = inputLeft;
     }
-    
+
     // Sets the right child of this TreeNode
     //
     // Precondition:
@@ -142,13 +142,13 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
     // b) A TreeNode, inputRight, is given as input.
     // Postcondition:
     // a) The right child of this TreeNode is now the given inputRight.
-    
+
     public void setRight(TreeNode inputRight) {
       rightChild = inputRight;
     }
-  
+
   }
-  
+
   // TrreMinHeap Invariant:
   // a) root is the root of a binary tree representing a MinHeap, whose nodes store
   //    non=null values from the ordered type T. Thus this binary tree has the shape of
@@ -157,13 +157,13 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
   //    left and right children, if these exist.
   // b) heapSize is the current size of this binary heap.
   // c) latest is the existing node that was most recently added to this MinHeap.
-  
-  
+
+
   // Data Fields
   private int heapSize;     // The size of this MinHeap
   private TreeNode root;    // The root of the binary tree representing this MinHeap
   private TreeNode latest;  // The last remaining TreeNode added to this MaxHeap
-  
+
   /**
   *
   * Creates an empty MinHeap.<br><br>
@@ -173,14 +173,14 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
   *  is an empty heap, so that its heapSize is zero.
   *
   */
-  
+
   public TreeMinHeap () {
     heapSize = 0;
     root = null;
     latest = null;
-  
+
   }
-  
+
   // Returns the existing node most recently added before "latest" or null, if
   // no such node exists.
   //
@@ -188,20 +188,20 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
   //   is not empty.
   // Postcondition: The TreeNode added most recently before latest is returned
   //   as output.
-  
+
   private TreeNode predecessor () {
 	  //int i = latest.getIndex() -1;
-	  
+
     return root.getRight();
   }
-  
+
   // Returns the node that should become the parent of the next node to be added.
   //
   // Precondition: The TreeMinHeap Invariant is satisfied, and the MinHeap represented
   //   is not empty.
   // Postcondition: The TreeNode that should be the parent of the next node to be
   //   added is returned.
-  
+
   private TreeNode successorParent ()  {
 	  if(latest==null) {
 		  return null;
@@ -210,7 +210,7 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
 		  return root;
 	  }
 	  if(latest==latest.getParent().getLeft()) {
-		  return latest.getParent(); 
+		  return latest.getParent();
 	  }else {
 		 TreeNode x = latest.getParent();
 		 boolean left=false;
@@ -229,11 +229,11 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
 			 }
 			 return x;
 		 }
-		
+
 	  }
-  
+
   }
-  
+
   //
   // Implements the "bubbleUp" method needed to complete an insertion
   //
@@ -251,11 +251,15 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
   //    by the nodes in this binary tree.
   // b) This binary tree is now a representation of a binary MinHeap.
   //
-  
+
   private void bubbleUp(TreeNode x) {
-	  //remeber to update latest 
+    while (x.getParent() >= 0 && x.getParent().compareTo(x.getValue()) > 0) {
+      T temp = x.getValue();
+      x.setValue(x.getParent());
+      x.setParent(temp);
+    }
   }
-  
+
   //
   // Implements the "bubbleDown" method used to complete a deletion
   //
@@ -272,13 +276,26 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
   //    between nodes but otherwise unchanged - the same multiset is represented
   //    by the nodes in this binary tree.
   // b) This binary tree is now a representation of a binary MinHeap.
-  
+
   private void bubbleDown(TreeNode x) {
-  
+    while(x.getLeft() != null) {
+      T small = x.getLeft();
+
+      if (x.getRight() != null && x.getLeft().compareTo(x.getRight()) > 0) {
+        small = x.getRight();
+      }
+      if (x.getValue().compareTo(small) > 0) {
+        T temp = x.getValue();
+        x.setValue(small);
+        x.setParent(temp);
+      } else {
+          break;
+      }
+    }
     // To be supplied by students
-    
+
   }
-  
+
   // Implementation of the insert method provided by a MinHeap. The preconditions
   // and postcondition for this problem are the same, except that they now also
   // include the fact that the "TreeMinHeap Invariant" is satisfied when execution
@@ -291,39 +308,39 @@ public class TreeMinHeap<T extends Comparable<T>> implements MinHeap<T> {
 	  bubbleUp(x);
 	  //latest= x;
   }
-  
+
   // Implementation of the deleteMin method provided by a MinHeap. The precondition
   // and postcondition for this problem are the same, except that they now also
   // include the fact that the "TreeMinHeap Invariant" is satisfied when execution
   // of the algorithm begins, and when it ends.
 
   public T deleteMin () throws NoSuchElementException {
-  
+    
     return null;   // To be supplied by students
 
   }
-  
+
   public int getSize() {
-  
+
     return heapSize;
-  
+
   }
-  
+
   // Used for Testing: Returns the root of the bree used to represent this
   // binary MinHeap
-  
+
   TreeNode getRoot() {
-  
+
     return root;
-  
+
   }
-  
+
   // Used for Testing: Returns the value of latest
-  
+
   TreeNode getLatest() {
-  
+
     return latest;
-  
+
   }
 
 }
